ef375d5b4d33083bd378df9c4feee5d788d6d01b
Phuc Luu <ac164529@avcol.school.nz>
2026-04-23 11:11:06 +1200

    fixed Database

--- VOSG-NKBD/Areas/Identity/Data/DatabaseSeed.cs
+++ VOSG-NKBD/Areas/Identity/Data/DatabaseSeed.cs
@@ -29,11 +29,11 @@ namespace VOSG_NKBD.Areas.Identity.Data
             // Seed Locations 
             var locations = new Location[]
             {
-                new Location { LocationName = "Auckland Showgrounds", Addresss = "217 Green Lane West", Suburb = "Epsom", City = "Auckland", Country = "New Zealand", PostalCode = "1051", PhoneNumber = "+64 9-623-0092" },
-                new Location { LocationName = "Nathan Phillips Square", Addresss = "100 Queen Street West", Suburb = "Central", City = "Toronto", Country = "Canada", PostalCode = "M5H 2N2", PhoneNumber = "+1 416-392-2489" },
-                new Location { LocationName = "Dr. Phillips Center for the Performing Arts", Addresss = "445 South Magnolia Avenue", Suburb = "Central", City = "Orlando", Country = "The USA", PostalCode = "FL 32801", PhoneNumber = "+1 407-358-6603" },
-                new Location { LocationName = "Federation Square", Addresss = "Swanston Street & Flinders Street", Suburb = "Central", City = "Melbourne", Country = "Australia", PostalCode = "VIC 3000", PhoneNumber = "+61 3 9655 1900" },
-                new Location { LocationName = "Finlandia Hall", Addresss = "Mannerheimintie 13", Suburb = "Central", City = "Helsinki", Country = "Finland",  PostalCode = "00100", PhoneNumber = "+358 9 40241" }
+                new Location { LocationName = "Auckland Showgrounds",                        Addresss = "217 Green Lane West",               Suburb = "Epsom",   City = "Auckland",  Country = "New Zealand", PostalCode = "1051",     PhoneNumber = "+64 9-623-0092" },
+                new Location { LocationName = "Nathan Phillips Square",                      Addresss = "100 Queen Street West",             Suburb = "Central", City = "Toronto",   Country = "Canada",      PostalCode = "M5H 2N2",  PhoneNumber = "+1 416-392-2489" },
+                new Location { LocationName = "Dr. Phillips Center for the Performing Arts", Addresss = "445 South Magnolia Avenue",         Suburb = "Central", City = "Orlando",   Country = "The USA",     PostalCode = "FL 32801", PhoneNumber = "+1 407-358-6603" },
+                new Location { LocationName = "Federation Square",                           Addresss = "Swanston Street & Flinders Street", Suburb = "Central", City = "Melbourne", Country = "Australia",   PostalCode = "VIC 3000", PhoneNumber = "+61 3 9655 1900" },
+                new Location { LocationName = "Finlandia Hall",                              Addresss = "Mannerheimintie 13",                Suburb = "Central", City = "Helsinki",  Country = "Finland",     PostalCode = "00100",    PhoneNumber = "+358 9 40241" }
             };
             context.Locations.AddRange(locations);
             await context.SaveChangesAsync();
@@ -41,11 +41,11 @@ namespace VOSG_NKBD.Areas.Identity.Data
             // Seed Users via UserManager
             var usersToCreate = new (string FirstName, string LastName, string Email, string Phone)[]
             {
-                ("Nicolas", "Jackson", "NicolasJackson@gmail.com", "+64-02-783-21892"),
-                ("Jenny", "Monroe", "JennyMonroe@gmail.com", "+1-416-123-4567"),
-                ("Alice", "Sydney", "AliceSydney@gmail.com", "+1-407-123-4567"),
-                ("Bobby", "Gordon", "BobbyGordon@gmail.com", "+61-412-345-678"),
-                ("Emma", "Hathaway", "EmmaHathaway@gmail.com", "+358-41-123-4567")
+                ("Nicolas", "Jackson",  "NicolasJackson@gmail.com", "+64-02-783-21892"),
+                ("Jenny",   "Monroe",   "JennyMonroe@gmail.com",    "+1-416-123-4567"),
+                ("Alice",   "Sydney",   "AliceSydney@gmail.com",    "+1-407-123-4567"),
+                ("Bobby",   "Gordon",   "BobbyGordon@gmail.com",    "+61-412-345-678"),
+                ("Emma",    "Hathaway", "EmmaHathaway@gmail.com",   "+358-41-123-4567")
             };
 
             var createdUsers = new List<VOSG_NKBDUser>();
